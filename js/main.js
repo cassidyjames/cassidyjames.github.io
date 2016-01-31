26731eb56642f21c7cfec05912da98e0f6c3fbcd
Daniel Foré <daniel@elementary.io>
2016-01-30 16:35:42 -0800

    just use one js file

--- js/main.js
+++ js/main.js
@@ -1,4 +1,11 @@
 $(function () {
     $('#year').text((new Date).getFullYear ());
+
+    $('a').each(function () {
+        var href = this.href;
+        if ( href.indexOf(window.location.host) == -1 ) {
+            $(this).attr('target', '_blank');
+        }
+    });
 });
 
