aa239f9b53cef4fc411dce73679fc0a067fe7bfb
Daniel Foré <daniel@elementary.io>
2016-01-31 11:32:24 -0800

    more things

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 $(function () {
     $('#year').text((new Date).getFullYear ());
 
+    // Open external ;inks in a new tab
     $('a').not('[href*="mailto:"]').each(function () {
         var href = this.href;
         if ( href.indexOf(window.location.host) == -1 ) {
@@ -8,8 +9,14 @@ $(function () {
         }
     });
 
+    // How old am I?
     var diff = new Date () - new Date ("1989/7/30");
     var age = Math.floor (diff/31536000000);
     $('#age').text (age);
+
+    // How many years playing guitar?
+    var diff = new Date () - new Date ("2003/7/30");
+    var age = Math.floor (diff/31536000000);
+    $('#guitar-age').text (age);
 });
 
