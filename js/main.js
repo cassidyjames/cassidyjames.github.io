21570aec62cc88a1a8f86da4aa5476c88b6d6c25
Daniel Foré <daniel@elementary.io>
2016-03-13 13:24:58 -0700

    years since as a function

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 $(function () {
     $('#year').text((new Date).getFullYear ());
 
-    // Open external ;inks in a new tab
+    // Open external links in a new tab
     $('a').not('[href*="mailto:"]').each(function () {
         var href = this.href;
         if ( href.indexOf(window.location.host) == -1 ) {
@@ -10,13 +10,14 @@ $(function () {
     });
 
     // How old am I?
-    var diff = new Date () - new Date ("1989/7/30");
-    var age = Math.floor (diff/31536000000);
-    $('#age').text (age);
+    $('#age').text (yearsSince ("1989/7/30"));
 
     // How many years playing guitar?
-    var diff = new Date () - new Date ("2003/7/30");
-    var age = Math.floor (diff/31536000000);
-    $('#guitar-age').text (age);
+    $('#guitar-age').text (yearsSince ("2003/7/30"));
 });
 
+function yearsSince (epoch) {
+    var diff = new Date () - new Date (epoch);
+    var age = Math.floor (diff/31536000000);
+    return age;
+}
