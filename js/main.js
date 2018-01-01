2a332e4ee770aca91cbd85629f5f99c5089b1dc3
Cassidy James Blaede <c@ssidyjam.es>
2017-12-31 23:22:15 -0700

    Fix typo in comment

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 $(function () {
-  // Open external ;inks in a new tab
+  // Open external links in a new tab
   $('a').not('[href*="mailto:"]').each(function () {
     var href = this.href;
     if ( href.indexOf(window.location.host) == -1 ) {
