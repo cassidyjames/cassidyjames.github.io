b7968b2169fba00ce9a301e61ee226d29e2a44b5
Aaron Honeycutt <aaronhoneycutt@pm.me>
2021-08-24 19:01:08 -0600

    more clean up

--- downloads/Ghibli Wallpapers.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-for movie in marnie kaguyahime kazetachinu kokurikozaka karigurashi ponyo ged chihiro
-do
-  for i in {001..050}
-  do
-    wget "http://www.ghibli.jp/gallery/$movie$i.jpg"
-  done
-done
--- downloads/Ghibli Wallpapers.zip.md
+++
@@ -1,3 +0,0 @@
----
-redirect_to: https://mega.nz/file/KsgATT4C#Wn_NuPj62N93J6Fg4HQ8-vb5gLdNnNQmAHI8i5kiYHA
----
